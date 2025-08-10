78fb41adb1b64ee115751bcc051eea26d0343a05
WOLF <blackwolf3466@gmail.com>
2025-08-10 09:34:08 +0300

    feat: enhance note management by adding addNote and deleteNote functions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,12 +5,28 @@ const getNotes = () => {
     return notes;
 };
 
+const addNote = (title, desc) => {
+    const notes = getNotes();
+    notes.push({ title, desc });
+    localStorage.setItem("notes", JSON.stringify(notes));
+    renderNotes();
+};
+
+const deleteNote = (index) => {
+    const notes = getNotes();
+    notes.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notes));
+    renderNotes();
+};
+
 const renderNotes = () => {
     if (getNotes().length === 0) {
         noNotesContainer.style.display = "block";
     } else {
         noNotesContainer.style.display = "none";
-    }
+    };
+
+    
 }
 
 renderNotes();
